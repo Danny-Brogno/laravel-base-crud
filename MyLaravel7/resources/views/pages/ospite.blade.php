da1c2d1f51c7a52a88f4418e2a4ba6b7545f5c5e
Danny <danny.brogno@gmail.com>
2021-05-25 18:51:23 +0200

    progression

--- MyLaravel7/resources/views/pages/ospite.blade.php
+++ MyLaravel7/resources/views/pages/ospite.blade.php
@@ -1,7 +1,17 @@
 @extends("Layouts.main-layout")
   @section("content")
+
   <div class="ospite-container">
     <h3>{{ $ospite -> name }}</h3>
     <h3>{{ $ospite -> lastname }}</h3>
   </div>
+
+  <section class="otherInfo">
+    <h3>Date of birth: &#160;{{ $ospite -> date_of_birth }}</h3>
+    <h3>Document type: &#160;{{ $ospite -> document_type }}</h3>
+    <h3>Document number: &#160;{{ $ospite -> document_number }}</h3>
+    <h3>Created on the: &#160;{{ $ospite -> created_at }}</h3>
+    <h3>Updated on the: &#160;{{ $ospite -> updated_at }}</h3>
+  </section>
+
   @endsection
